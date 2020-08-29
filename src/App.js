33c0b3815bc8f000e075ff6ae760a7c8bae85fc4
twest3 <twest3@snapchat.com>
2020-08-28 23:45:45 -0700

    use context provider for user info

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {
-  SignUp,
-  Login,
-  Logout,
-  NotFound,
-  Landing,
-  Dashboard,
-} from './pages/index';
+import { SignUp, Login, NotFound, Landing, Dashboard } from './pages/index';
 import Navbar from './components/Navbar';
+import { UserContext } from './UserContext';
 
 export default function App() {
+  const [data, setData] = useState({ pokemon: [] });
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/');
+      setData(data);
+    };
+    fetchData();
+  }, []);
+
+  const [user, setUser] = useState('tyroo');
+  const currentUser = useMemo(() => ({ user, setUser }), [user, setUser]);
+
   return (
     <>
       <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' render={() => <Landing />} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/signup' render={() => <SignUp />} />
-          <Route exact path='/dashboard' component={Dashboard} />
-          <Route exact path='/logout' component={Logout} />
-          <Route component={NotFound} />
-        </Switch>
+        <UserContext.Provider value={currentUser}>
+          <Navbar />
+          <Switch>
+            <Route exact path='/dashboard' component={Dashboard} />
+            <Route exact path='/' render={() => <Landing />} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' render={() => <SignUp />} />
+            <Route component={NotFound} />
+          </Switch>
+        </UserContext.Provider>
       </Router>
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -7,6 +7,7 @@ import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { NoMeetingRoom, MeetingRoom, Home } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../UserContext';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -19,29 +20,38 @@ const useStyles = makeStyles(() => ({
 
 export default function ButtonAppBar() {
   const classes = useStyles();
+  const { user, setUser } = useContext(UserContext);
 
   return (
     <div className={classes.root}>
       <AppBar position='static' color='secondary'>
         <Toolbar>
           <Typography variant='h6' className={classes.title}>
-            Pokedex Project
+            {user ? user : 'Pokedex Project'}
           </Typography>
           <Button color='inherit'>
             <Link to='/dashboard'>
               Home <Home />
             </Link>
           </Button>
-          <Button color='inherit'>
-            <Link to='/login'>
-              Login <MeetingRoom />
-            </Link>
-          </Button>
-          <Button color='inherit'>
-            <Link to='/logout'>
-              Logout <NoMeetingRoom />
-            </Link>
-          </Button>
+          {user ? (
+            ''
+          ) : (
+            <Button color='inherit'>
+              <Link to='/login'>
+                Login <MeetingRoom />
+              </Link>
+            </Button>
+          )}
+          {!user ? (
+            ''
+          ) : (
+            <Button onClick={() => setUser(null)} color='inherit'>
+              <Link to='/'>
+                Logout <NoMeetingRoom />
+              </Link>
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
       <CssBaseline />
