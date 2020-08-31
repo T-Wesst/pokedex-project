023e92eb0129f2363da7dcc6f8b3be3dd159bb8e
twest3 <twest3@snapchat.com>
2020-08-30 21:31:42 -0700

    style login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,26 +1,125 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import axios from 'axios';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import Person from '@material-ui/icons/Person';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
 
-export default function Login() {
-  const { handleSubmit, register, errors } = useForm();
+const styles = (theme) => ({
+  main: {
+    width: 'auto',
+    display: 'block',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${
+      theme.spacing.unit * 3
+    }px`,
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing.unit,
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3,
+  },
+});
+
+function Login({ classes }) {
+  const { handleSubmit, register } = useForm();
   const onSubmit = (data) => console.log(data);
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input type='email' placeholder='email' name='email' ref={register} />
-        <input
-          type='password'
-          placeholder='password'
-          name='password'
-          ref={register({
-            required: 'PASSWORD REQUIRED',
-            minLength: { value: 8, message: 'PASSWORD LENGTH IS TOO SHORT' },
-          })}
-        />
-        {errors.password && <p>{errors.password.message}</p>}
-        <input type='submit' />
-      </form>
-    </>
+    <main className={classes.main}>
+      <CssBaseline />
+      <Paper className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <Person />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Log in
+        </Typography>
+        <form onSubmit={handleSubmit(onSubmit)} className={classes.form}>
+          <FormControl margin='normal' required fullWidth>
+            <InputLabel htmlFor='email'>Email Address</InputLabel>
+            <Input
+              id='email'
+              type='email'
+              name='email'
+              inputRef={register}
+              autoComplete='email'
+              autoFocus
+            />
+          </FormControl>
+          <FormControl margin='normal' required fullWidth>
+            <InputLabel htmlFor='password'>Password</InputLabel>
+            <Input
+              name='password'
+              type='password'
+              id='password'
+              autoComplete='current-password'
+              inputRef={register({ require: true, minLength: 8 })}
+            />
+          </FormControl>
+          <FormControlLabel
+            control={
+              <Checkbox
+                inputRef={register}
+                name='remember'
+                color='secondary'
+                defaultValue={false}
+              />
+            }
+            label='Remember me'
+          />
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            color='secondary'
+            className={classes.submit}
+          >
+            Sign in
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href='#' variant='body2'>
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href='#' varaint='body2'>
+                {'Dont have an account? Sign Up'}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </main>
   );
 }
+
+export default withStyles(styles)(Login);
