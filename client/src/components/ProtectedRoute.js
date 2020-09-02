e8092d863ab8ce65b1b95e3c1cc0f0aa0a9f3b45
twest3 <twest3@snapchat.com>
2020-09-01 17:20:15 -0700

    add protected route

---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import { UserContext } from '../UserContext';
+import { Route, Redirect } from 'react-router-dom';
+
+export default function ProtectedRoute({ component: Component, ...rest }) {
+  const { user } = useContext(UserContext);
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (user) {
+          return <Component {...props} />;
+        } else {
+          return <Redirect to='/login' />;
+        }
+      }}
+    />
+  );
+}
