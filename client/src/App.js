dbaa29bccda92a0c8a07364162256cb8a6de90dc
twest3 <twest3@snapchat.com>
2020-09-02 00:30:25 -0700

    fetch pokemon

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,39 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import axios from 'axios';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { SignUp, Login, NotFound, Landing, Dashboard } from './pages/index';
 import Navbar from './components/Navbar';
 import { UserContext } from './UserContext';
 import ProtectedRoute from './components/ProtectedRoute';
+import axios from 'axios';
 
 export default function App() {
-  const [data, setData] = useState({ pokemon: [] });
+  const [pokemonData, setPokemonData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [nextPage, setNextPage] = useState('');
+  const [prevPage, setPrevPage] = useState('');
+  const baseURL = 'https://pokeapi.co/api/v2/pokemon/';
+
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/');
-      setData(data);
+      const { data } = await axios.get(baseURL);
+      setNextPage(data.next);
+      setPrevPage(data.previous);
+      setIsLoading(false);
+      await loadPokemon(data.results);
     };
     fetchData();
   }, []);
+
+  const loadPokemon = async (data) => {
+    let pokeData = await Promise.all(
+      data.map(async (pokemon) => {
+        return await axios.get(pokemon.url);
+      })
+    );
+    setPokemonData(pokeData);
+  };
+
+  console.log(pokemonData);
 
   const [user, setUser] = useState(null);
   const currentUser = useMemo(() => ({ user, setUser }), [user, setUser]);
