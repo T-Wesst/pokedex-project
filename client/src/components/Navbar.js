fb1ba2003c1bd98820768998c99fe1656808e035
twest3 <twest3@snapchat.com>
2020-09-03 00:07:59 -0700

    add search bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,66 @@
 import React, { useContext } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles, fade } from '@material-ui/core/styles';
+import { AppBar, Toolbar, InputBase } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { NoMeetingRoom, MeetingRoom, Home } from '@material-ui/icons';
+import SearchIcon from '@material-ui/icons/Search';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../UserContext';
 import axios from 'axios';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
   title: {
     flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
   },
 }));
 
@@ -39,24 +84,34 @@ export default function ButtonAppBar() {
             {user ? `Welcome ${user}` : 'Pokedex Project'}
           </Typography>
           {user ? (
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder='Search…'
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </div>
+          ) : null}
+          {user ? (
             <Button color='inherit'>
-              <Link to='/dashboard'>
-                Home <Home />
-              </Link>
+              <Link to='/dashboard'>Home</Link>
             </Button>
           ) : null}
+
           {user ? null : (
             <Button color='inherit'>
-              <Link to='/login'>
-                Login <MeetingRoom />
-              </Link>
+              <Link to='/login'>Login</Link>
             </Button>
           )}
           {!user ? null : (
             <Button onClick={logout} color='inherit'>
-              <Link to='/'>
-                Logout <NoMeetingRoom />
-              </Link>
+              <Link to='/'>Logout</Link>
             </Button>
           )}
         </Toolbar>
